f91d9530b00870ba152baa884b7ba74074d6382a
陈绪源 <chenxuyuan@chenxuyuandeMacBook-Pro.local>
2018-07-22 17:08:25 -0400

    Converted all spaces into tabs

--- app/db/migrations/201807190254.js
+++ app/db/migrations/201807190254.js
@@ -4,50 +4,50 @@ module.exports = {
 
 up: function (queryInterface, Sequelize) {
 	return [
-      queryInterface.addColumn(
-      	'users',
-        'user_type',
-        {
-          type: Sequelize.STRING,
-          allowNull: false
-        }
-      ),
-      queryInterface.addColumn(
-      	'users',
-        'phone',
-        {
-          type: Sequelize.STRING,
-          allowNull: false
-        }
-      ),
-      queryInterface.addColumn(
-      	'users',
-        'preferred_name',
-        {
-          type: Sequelize.STRING,
-          allowNull: false
-        }
-      ),
-      queryInterface.removeColumn(
-        'users',
-        'middle_name',
-      )
-    ];
+		queryInterface.addColumn(
+		'users',
+		'user_type',
+		{
+			type: Sequelize.STRING,
+			allowNull: false
+		}
+		),
+		queryInterface.addColumn(
+		'users',
+		'phone',
+		{
+			type: Sequelize.STRING,
+			allowNull: false
+		}
+	  ),
+		queryInterface.addColumn(
+		'users',
+		'preferred_name',
+		{
+			type: Sequelize.STRING,
+			allowNull: false
+		}
+		),
+		queryInterface.removeColumn(
+		'users',
+		'middle_name',
+		)
+	];
 },
 
 down: function (queryInterface, Sequelize) {
-    return [
-      queryInterface.addColumn(
-        'users',
-        'middle_name',
-        {
-          type: Sequelize.STRING,
-          allowNull: false
-        }
-      ),
-      queryInterface.removeColumn('users', 'preferred_name'),
-      queryInterface.removeColumn('users', 'phone'),
-      queryInterface.removeColumn('users', 'user_type')
-    ];
-  }
+	return [
+		queryInterface.addColumn(
+		'users',
+		'middle_name',
+		{
+			type: Sequelize.STRING,
+			allowNull: false
+		}
+		),
+		queryInterface.removeColumn('users', 'preferred_name'),
+		queryInterface.removeColumn('users', 'phone'),
+		queryInterface.removeColumn('users', 'user_type')
+	];
+	}
 };
